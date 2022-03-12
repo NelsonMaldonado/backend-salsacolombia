e94a111cad74542f0648093a9e0776a63c0cd9ca
Nelson Maldonado <play2nino@gmail.com>
2022-03-11 23:35:43 -0800

    added a catch error helper function

--- api/catalog/catalog-router.js
+++ api/catalog/catalog-router.js
@@ -38,4 +38,11 @@ router.post("/", async (req, res, next) => {
   }
 })
 
+router.use((err, req, res, next) => {
+  res.status(err.status || 500).json({
+    message: err.message,
+    stack: err.stack,
+  })
+})
+
 module.exports = router
